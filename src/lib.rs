6daa07d260f178d5a8fd4512ca6cd9c8cd9a3598
Nitzan Elbaz <nitzan@claysciences.com>
2018-06-29 00:34:20 +0300

    many tries and many fails. ioctl fails (ithink)

--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,150 @@
 #![deny(missing_docs)]
 #![feature(libc)]
 extern crate libc;
-use libc::{c_int, c_void, sockaddr_storage, socket, AF_PACKET};
+use libc::{c_int, c_void, sockaddr_storage, socket, PF_PACKET};
 
 use std::mem;
 use std::io;
 
-static SOCK_RAW: c_int = 3;
-static IPPROTO_ICMP: c_int = 1;
+const SOCK_RAW: c_int = 3;
+const IPPROTO_ICMP: c_int = 1;
+const IFNAMESIZE: usize = 16;
+const IFREQUNIONSIZE: usize = 24;
+
 
 struct RawSocket {
     handle: i32,
 }
 
+#[repr(C)]
+struct IfReqUnion {
+    data: [u8; IFREQUNIONSIZE],
+}
+
+impl Default for IfReqUnion {
+    fn default() -> IfReqUnion {
+        IfReqUnion { data: [0; IFREQUNIONSIZE] }
+    }
+}
+
+impl IfReqUnion {
+    fn as_sockaddr(&self) -> libc::sockaddr {
+        let mut addr = libc::sockaddr {
+            sa_family: u16::from_be((self.data[0] as u16) << 8 | (self.data[1] as u16)),
+            sa_data: [0; 14],
+        };
+
+        for (i, b) in self.data[2..16].iter().enumerate() {
+            addr.sa_data[i] = *b as i8;
+        }
+
+        addr
+    }
+
+    fn as_int(&self) -> libc::c_int {
+        libc::c_int::from_be((self.data[0] as libc::c_int) << 24|
+                             (self.data[1] as libc::c_int) << 16|
+                             (self.data[2] as libc::c_int) << 8|
+                             (self.data[3] as libc::c_int))
+    }
+
+    fn as_short(&self) -> libc::c_short {
+        libc::c_short::from_be((self.data[0] as libc::c_short) << 8 |
+                               (self.data[1] as libc::c_short))
+    }
+}
+
+#[repr(C)]
+struct IfReq {
+    ifr_name: [libc::c_char; IFNAMESIZE],
+    union: IfReqUnion,
+}
+
+impl Default for IfReq {
+    fn default() -> IfReq {
+        IfReq {
+            ifr_name: [0; IFNAMESIZE],
+            union: IfReqUnion::default(),
+        }
+    }
+}
+
+impl IfReq {
+    /// Create an interface request struct with the interace name set
+    pub fn with_if_name(if_name: &str) -> io::Result<IfReq> {
+        let mut if_req = IfReq::default();
+
+        if if_name.len() >= if_req.ifr_name.len() {
+            return Err(io::Error::new(io::ErrorKind::Other, "Interface name too long."));
+        }
+
+        for (a, c) in if_req.ifr_name.iter_mut().zip(if_name.bytes()) {
+            *a = c as i8;
+        }
+
+        Ok(if_req)
+    }
+
+    pub fn ifr_hwaddr(&self) -> libc::sockaddr {
+        self.union.as_sockaddr()
+    }
+
+    pub fn ifr_broadaddr(&self) -> libc::sockaddr {
+        self.union.as_sockaddr()
+    }
+
+
+    pub fn ifr_ifindex(&self) -> libc::c_int {
+        self.union.as_int()
+    }
+
+    pub fn ifr_media(&self) -> libc::c_int {
+        self.union.as_int()
+    }
+
+    pub fn ifr_flags(&self) -> libc::c_short {
+        self.union.as_short()
+    }
+}
+
 
 impl RawSocket {
     /// Create a new raw socket.
-    fn new() -> io::Result<Self> {
-        let handle = unsafe { socket(AF_PACKET, SOCK_RAW, libc::ETH_P_ALL.to_be() as i32) };
-        match handle {
-            -1 => Err(io::Error::last_os_error()),
-            _ => Ok ( RawSocket { handle }),
+    fn new(interface: &str) -> io::Result<Self> {
+        let handle = unsafe { socket(PF_PACKET, SOCK_RAW, libc::ETH_P_ALL.to_be() as i32) };
+
+        if handle == -1 {
+            return Err(io::Error::last_os_error());
         }
+
+        let sock = RawSocket { handle };
+
+        let if_req = IfReq::with_if_name(interface)?;
+        let mut req: Box<IfReq> = Box::new(if_req);
+        let SIOCGIFINDEX = 0x8933;
+
+        if unsafe { libc::ioctl(sock.handle, SIOCGIFINDEX, &mut *req) } == -1 {
+            return Err(io::Error::last_os_error());
+        }
+
+        let mut sll : libc::sockaddr_ll = unsafe {mem::zeroed()};
+
+
+        sll.sll_family = libc::AF_PACKET as u16;
+        sll.sll_ifindex = req.ifr_ifindex();
+        sll.sll_protocol = libc::ETH_P_ALL.to_be() as u16;
+
+        unsafe {
+            let addr_ptr = mem::transmute::<*mut libc::sockaddr_ll, *mut libc::sockaddr>(&mut sll);
+            if libc::bind(
+                handle, 
+                addr_ptr as *mut libc::sockaddr,
+                mem::size_of::<libc::sockaddr_ll> as u32) == -1 {
+                return Err(io::Error::last_os_error());
+            }
+        }
+
+        Ok(sock)
     }
 }
 
@@ -64,7 +187,7 @@ mod tests {
     #[test]
     fn create_raw_socket() {
         use super::RawSocket;
-        RawSocket::new().expect("Create socket failed");
+        let sock = RawSocket::new("eno1").expect("Create socket failed");
     }
 
     #[test]
@@ -72,7 +195,7 @@ mod tests {
         use super::RawSocket;
         use std::io::Read;
         let mut bytes = [0;10];
-        let mut sock = RawSocket::new().expect("Create socket failed");
+        let mut sock = RawSocket::new("eno1").expect("Create socket failed");
         let result = sock.read(&mut bytes).unwrap();
         println!("{:#?}", bytes);
     }
